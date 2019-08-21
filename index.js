a1475da9eeffb507b9a097d3f480fd38fa574868
Sarath <frndsarath@gmail.com>
2019-08-21 22:14:05 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,61 @@
+var express = require('express');
+var expressHandebars = require('express-handlebars');
+
+var app = express();
+
+app.get('/', (req, res) => {
+  res.end('srt login works');
+});
+
+// app.use(express.static(__dirname + '/views'));
+
+var users = {
+  groups: {
+    admin: ['eecha', 'mappy', 'anu'],
+    members: ['anju', 'parvathy', 'surya']
+  }
+};
+
+app.engine('handlebars', expressHandebars({ defaultLayout: 'main' }));
+
+app.set('view engine', 'handlebars');
+
+app.get('/home', (req, res) => {
+  res.end('You are home!!');
+});
+
+app.get('/admins/:t?', (req, res) => {
+  if (req.params.t == 'html') {
+    res.render('response', {
+      type: 'Admin',
+      names: users.groups.admin
+    });
+  } else {
+    res.end(JSON.stringify(users.groups.admin));
+  }
+});
+
+app.get('/members/:t?', (req, res) => {
+  if (req.params.t == 'html') {
+    res.render('response', {
+      type: 'Member',
+      names: users.groups.members
+    });
+  } else {
+    res.end(JSON.stringify(users.groups.members));
+  }
+});
+
+app.get('/*', (req, res) => {
+  // res.writeHead(404);
+  res.render('error', {
+    errorType: '404',
+    errorMessage: 'Not Available Dude!!'
+  });
+  // res.end('Not Available Dude');
+});
+
+var port = '3456';
+app.listen(port, () => {
+  console.log('Server started on port : ' + port);
+});
